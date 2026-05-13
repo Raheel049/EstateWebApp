f241dd499366f3dda4e7af4659edc9f84fb0068b
Raheel Ahmed <raheel25031@gmail.com>
2026-05-13 17:17:50 +0500

    Prisma setup success

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,14 @@
-export const register = (req, res) => {
-    console.log("register auth")
+import bcrypt from "bcrypt"
+
+export const register = async (req, res) => {
+    console.log("register body",req.body);
+
+    const {username, email, password} = req.body
+
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    console.log("hash",hashPassword)
+
 }
 
 export const login = (req, res) => {
