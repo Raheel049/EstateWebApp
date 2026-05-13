1ffad4636663940abf86d8b3192a853340d354d1
Raheel Ahmed <raheel25031@gmail.com>
2026-05-13 20:20:31 +0500

    Login api created

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,9 @@
 import bcrypt from "bcrypt"
+import prisma from "../lib/prisma.js";
 
 export const register = async (req, res) => {
-    console.log("register body",req.body);
+    try {
+        console.log("register body",req.body);
 
     const {username, email, password} = req.body
 
@@ -9,10 +11,49 @@ export const register = async (req, res) => {
 
     console.log("hash",hashPassword)
 
+    const newUser = await prisma.user.create({
+        data : {
+            username,
+            email,
+            password : hashPassword,
+        },
+    });
+
+    console.log("newUser", newUser);
+
+    res.status(201).json({
+        message : "User created successfully",
+    })
+
+    } catch (error) {
+        res.status(500).json({
+            message : error.message || "Internal server error",
+        })
+    }
+
 }
 
-export const login = (req, res) => {
-    console.log("login auth")
+export const login = async (req, res) => {
+    try {
+        console.log("login auth")
+
+    const {username, password} = req.body
+
+    const user = await prisma.user.findUnique({
+        where:{username}
+    })
+
+    if(!user) return res.status(401).json({message : "invalid Credendials!"});
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if(!isPasswordValid) return res.status(401).json({message : "invalid Credendials!"});
+
+    } catch (error) {
+        console.log(error)
+
+        res.status(500).json({message : "Failed to login!"})
+    }
     
 }
 
